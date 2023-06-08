ea373e5f667911ffcf297efdca2847509e2e6b98
hannahwagner1402 <92428401+hannahwagner1402@users.noreply.github.com>
2023-06-08 15:05:59 +0200

    fixing up images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="home">
-    <NavBar />
+    <NavBar/>
     <v-container fluid>
       <div class="head">
         <v-row>
@@ -9,14 +9,15 @@
               <h1 class="intro">Hi! I'm Hannah Wagner, </h1>
               <h1 class="skills">Web Designer & Developer!
               </h1>
-              <h4 class ="facts"> Ambitious Creative Computing Student at the UAS St. Pölten based in Vienna Austria :)</h4>
+              <h4 class="facts"> Ambitious Creative Computing Student at the UAS St. Pölten based in Vienna Austria
+                :)</h4>
 
 
             </div>
           </v-col>
           <v-col cols="2">
             <div
-              style="
+                style="
                 position: absolute;
                 z-index: 9999;
                 bottom: 0;
@@ -26,134 +27,138 @@
                 right: 0;
                 text-align: center;
               "
-              class="mt-16"
+                class="mt-16"
             >
             </div>
           </v-col>
           <v-col cols="5">
-            <div style="position: relative; z-index: 9999" class="mt-12 align-center">
-             <div class="backgr">
-               <div class="imgCircle">
-              <v-img class="firstpic" src="slay.jpg" contain max-height="300"></v-img>
-               </div>
-             </div>
+            <div style="position: relative; z-index: 9999" class="mt-16 align-center">
+              <div class="backgr">
+                <div class="imgCircle">
+                  <v-img class="firstpic" src="intropic.JPG" contain max-height="400"></v-img>
+                </div>
+              </div>
             </div>
           </v-col>
         </v-row>
       </div>
 
-
       <v-col cols="12" class="mt-16" id="about">
         <div>
           <v-row>
-            <v-col cols="12" sm="6" class="mt-10">
-              <div class="egg">
-                <v-img src="seggsy.png" max-height="300" class=""></v-img>
-              </div>
+            <v-col cols="12" sm="4" class="mt-16">
+              <v-img src="AboutMe.JPG" max-height="400" class="mt-10"></v-img>
             </v-col>
-            <v-col cols="12" sm="6" class="mt-3">
+            <v-col cols="12" sm="4" class="mt-3">
               <h2 class="mt-16 mb-6">About Me</h2>
-              <div style="width: 120px">
-              </div>
-              <h4 class="mt-n4">I'm so slay pls hire me</h4>
+              <div style="width: 120px"></div>
 
-              <p class="text-grey mt-3">
-                My name is Hannah, and I am a student currently
-                pursuing web design and development. During my time studying,
-                I have acquired a diverse range of skills that enable me to excel
-                in both the coding and design aspects of the field. My coding skills
-                include proficiency in HTML, CSS, Javascript (with experience in Vue.js
-                and Node.js), Kotlin, and basic SQL.
-              </p>
-              <br />
               <p class="text-grey">
-                In terms of design, I am well-versed
-                in Adobe Photoshop, After Effects, Illustrator, and InDesign,
-                Procreate, and Figma. In addition, I have experience in
-                user research and user testing, making me knowledgeable in the field of
-                user experience. Outside of my studies, I enjoy drawing, illustrating, video editing
-                and working on my own projects. I am passionate about expanding my
-                knowledge and skills in coding, designing, and UI/UX, and am eager
-                to learn new things. Thank you for taking the time to learn a little bit
-                about me, and I look forward to the opportunity to work together in the future!
+                My name is Hannah, and I am a student currently pursuing web design and development. During my time studying, I have acquired a diverse range of skills that enable me to excel in both the coding and design aspects of the field. My coding skills include proficiency in HTML, CSS, Javascript (with experience in Vue.js and Node.js), Kotlin, and basic SQL.
+                <br>
+                In terms of design, I am well-versed in Adobe Photoshop, After Effects, Illustrator, and InDesign, Procreate, and Figma. In addition, I have experience in user research and user testing, making me knowledgeable in the field of user experience. Outside of my studies, I enjoy drawing, illustrating, video editing, and working on my own projects. I am passionate about expanding my knowledge and skills in coding, designing, and UI/UX, and am eager to learn new things. Thank you for taking the time to learn a little bit about me, and I look forward to the opportunity to work together in the future!
               </p>
+            </v-col>
+            <v-col cols="12" sm="4" class="mt-16">
+              <v-img src="AboutMe2.png" max-height="400" class="mt-10"></v-img>
             </v-col>
           </v-row>
         </div>
       </v-col>
 
+      <div class="dividerMargin">
+      <v-divider></v-divider>
+      </div>
+
       <v-col cols="12" class="padd" id="skillsset">
 
         <div class="text-center mt-16">
-        <h2 class="mySkills">MY SKILLZ 😎</h2>
-        <div style="width: 120px; margin: 0 auto">
+          <h2 class="mySkills">My Skillset</h2>
+          <div style="width: 120px; margin: 0 auto">
+          </div>
         </div>
-      </div>
-
-      <div class = "iconContainer">
-        <v-img class="skillIcons" src="js.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="css3-alt.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="vuejs.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="figma.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="html5.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="android.svg" contain max-height="300"></v-img>
-        <v-img class="skillIcons" src="node-js.svg" contain max-height="300"></v-img>
-      </div>
 
         <div class="first" id="project">
           <v-row>
             <v-col cols="12">
               <div class="child">
-                <v-btn
-                  icon="fa-solid fa-laptop"
-                  color="#97CF8A"
-                  class="text-white"
-                ></v-btn>
+                <img class="threeIcons" src="laptop.svg" alt="">
+
                 <h3 class="ml-3 mt-4">Web Development</h3>
                 <p class="text-grey ml-3 mt-4 text-caption">
-                  As a web developer, I design and develop <br>
-                  websites using various programming languages <br>
-                  such as HTML, CSS, and JavaScript.
+                  As a web developer, I design and <br>
+                  develop websites using various programming<br>
+                   languages such as HTML, CSS, and JavaScript.
                 </p>
               </div>
               <div class="child">
-                <v-btn
-                  icon="fas fa-mobile-alt"
-                  color="#97CF8A"
-                  class="text-white"
-                ></v-btn>
+                <img class="threeIcons" src="mobile.svg" alt="">
                 <h3 class="ml-3 mt-4">App Design</h3>
                 <p class="text-grey ml-3 mt-4 text-caption">
                   As an app designer, I specialize <br>
                   in creating visually appealing and <br>
-                  user-friendly mobile applications. <br />
+                  user-friendly mobile applications. <br/>
                 </p>
               </div>
               <div class="child">
-                <v-btn
-                  icon="fas fa-camera"
-                  color="#97CF8A"
-                  class="text-white"
-                ></v-btn>
+                <img class="threeIcons" src="uiux.svg" alt="">
                 <h3 class="ml-3 mt-4">UI/UX Design</h3>
                 <p class="text-grey ml-3 mt-4 text-caption">
-                  I prioritize creating enjoyable user experiences <br>
-                  and conduct user research and testing to ensure that<br>
-                  the product meets the needs of the target audience. <br>
+                  I prioritize creating enjoyable user  <br>
+                  experiences and conduct user research and<br>
+                  testing to help meet the needs of the users <br>
                 </p>
               </div>
             </v-col>
           </v-row>
-          <v-divider></v-divider>
-        </div>
-      </v-col>
 
+        </div>
+
+<h2 class="skillSeperator">Technologies used:</h2>
+        <div class="iconContainer">
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="js.svg" alt="JavaScript">
+            <h3 class="skillDescription">Javascript</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="css3-alt.svg" alt="CSS3">
+            <h3 class="skillDescription">CSS</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="vuejs.svg" alt="CSS3">
+            <h3 class="skillDescription">Vue.js</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="figma.svg" alt="CSS3">
+            <h3 class="skillDescription">Figma</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="html5.svg" alt="CSS3">
+            <h3 class="skillDescription">HTML</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="android.svg" alt="CSS3">
+            <h3 class="skillDescription">Kotlin</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="node-js.svg" alt="CSS3">
+            <h3 class="skillDescription">Node-js</h3>
+          </div>
+          <div class="skillIconWrapper">
+            <img class="skillIcons" src="unity.svg" alt="CSS3">
+            <h3 class="skillDescription">Unity</h3>
+          </div>
+
+        </div>
+
+        <v-divider></v-divider>
+      </v-col>
 
 
       <v-col cols="12" sm="12" id="projects">
 
         <div class="text-center mt-6">
-          <h2 class="mySkills">MY PROJECTS 🤭</h2>
+          <h2 class="mySkills">MY PROJECTS</h2>
           <div style="width: 120px; margin: 0 auto">
           </div>
         </div>
@@ -172,9 +177,9 @@
             <v-col cols="12" md="4">
               <v-hover v-slot="{ isHovering, props }">
                 <v-card
-                  :elevation="isHovering ? 12 : 2"
-                  :class="{ 'on-hover': isHovering }"
-                  v-bind="props"
+                    :elevation="isHovering ? 12 : 2"
+                    :class="{ 'on-hover': isHovering }"
+                    v-bind="props"
                 >
                   <v-img :src="item.img" height="225px" cover> </v-img>
                 </v-card>
@@ -187,29 +192,28 @@
         <hr class="v-divider">
       </v-col>
 
-
       <v-col cols="12" sm="12" class="px-16" id="contact">
         <v-row>
 
 
-            <div class="child2">
+          <div class="child2">
 
-              <h1 class="contact">Contact info</h1>
-              <h2 class="linc">Github</h2>
-              <h2 class="linc">LinkedIn</h2>
-              <h2 class="linc">Email</h2>
-              <h2 class="linc">Instagram</h2>
-            </div>
+            <h1 class="contact">Contact info</h1>
+            <h2 class="linc">Github</h2>
+            <h2 class="linc">LinkedIn</h2>
+            <h2 class="linc">Email</h2>
+            <h2 class="linc">Instagram</h2>
+          </div>
 
         </v-row>
       </v-col>
     </v-container>
-    <FooterView />
+    <FooterView/>
   </v-app>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 import NavBar from "../components/NavBar.vue";
 import FooterView from "../components/FooterView.vue";
 
@@ -257,7 +261,7 @@ export default defineComponent({
   components: {
     NavBar,
     FooterView
-},
+  },
 });
 </script>
 <style scoped>
@@ -265,51 +269,102 @@ export default defineComponent({
   padding: 16px 0 16px 0;
 }
 
-.mySkills{
-  margin-bottom: 30px;
-  font-family: "Baby Doll";
-  font-size: 50px;
+
+.skillSeperator {
+  text-align: center;
+}
+.threeIcons{
+  height: 80px;
+  width: 80px;
+  background-color: #B1DD9E;
+  padding: 15px;
+  border-radius: 30%;
 }
 
-.iconContainer{
-  margin: 0 auto;
+
+.dividerMargin{
+  margin-top: 35px;
+}
+.mySkills {
+  font-size: 45px;
+}
+
+.iconContainer {
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-wrap: wrap;
+  width:100%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+  margin-top: 60px;
+}
+.skillIconWrapper {
+  display: inline-block;
+  background-color: #E8E8E8;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  margin-right: 25px;
+  margin-bottom: 25px;
+  transition: transform 0.4s ease-in-out, background-color 0.4s ease-out;
+  transform-origin: bottom;
+}
+
+.skillIconWrapper:hover {
+  transform: scale(1.1, 1.1);
+  transition: background-color 0.4s ease-in-out;
+}
+
+.skillDescription {
+ font-size: 15px;
+  text-align: center;
+  color: #5F5F5F;
+  border-radius: 0px 0px 10px 10px;
+  transition: opacity 0.4s ease-in-out, background-color 0.4s ease-in-out;
+}
+
+.skillIcons {
+  max-height: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 66%;
-  height: 5%;
-}
-.skillIcons{
-  display: flex;
-  width: 100px;
-  height: 100px;
+  flex-wrap: wrap;
+  width:100px;
+  height:80px;
+  border-radius: 15px;
 }
 
-.intro{
+
+.intro {
   margin-left: 11%;
   display: flex;
   justify-content: flex-start;
   font-size: 2rem;
 }
 
-.skills{
+.skills {
   margin-left: 11%;
   margin-right: 0px;
   display: flex;
   justify-content: flex-start;
   font-size: 4rem;
+  line-height: 1.2;
+  padding-bottom: 16px;
+  padding-top: 10px;
 }
 
-.facts{
+.facts {
   margin-left: 11%;
   margin-right: 0px;
   display: flex;
   justify-content: flex-start;
-  color:darkgrey;
+  color: darkgrey;
 
 }
 
-.backgr{
+.backgr {
   background-color: #315A39;
   display: flex;
   justify-content: center;
@@ -318,14 +373,15 @@ export default defineComponent({
   height: 400px;
   overflow: hidden;
   border-radius: 50%;
+  margin-top: 70px;
 }
 
 .firstpic {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 300px;
-  height: 300px;
+  width: 350px;
+  height: 350px;
   overflow: hidden;
   border-radius: 50%;
 }
@@ -338,6 +394,7 @@ export default defineComponent({
   width: 100%;
   color: white;
 }
+
 .head:before {
   content: "";
   position: absolute;
@@ -348,6 +405,7 @@ export default defineComponent({
   background: black;
 
 }
+
 .head:after {
   content: "";
   position: absolute;
@@ -368,11 +426,13 @@ export default defineComponent({
   background-color: #B1DD9E;
   border-radius: 50% 50% 50% 50% / 60% 60% 50% 50%;
 }
+
 .first {
   width: 100%;
   height: 280px;
   text-align: center;
-  padding: 2rem 2rem;
+  padding: 1rem 1rem;
+  margin-bottom: 20px;
 }
 
 .child {
@@ -390,18 +450,22 @@ export default defineComponent({
   padding: 2rem 1rem;
   margin-right: 8px;
 }
-.contact{
-width: 100%;
+
+.contact {
+  width: 100%;
 }
-.linc{
+
+.linc {
   display: flex;
   justify-content: flex-end;
   margin-right: 10px;
   margin-left: 10px;
 }
+
 .imgHover {
   padding: 0 200px;
 }
+
 .pre {
   width: 100%;
   height: 380px;
